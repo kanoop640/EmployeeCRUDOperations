4ef97d5aa5629ef2fb1f0d0627142f9460b4eff9
Anoop kumar <kanoop6401@gmail.com>
2020-04-03 13:27:41 +0530

    [Anoop] Added test case that 1 inch and 1 Yard is not equal

---
+++ QuantatyMeasurement/YardToInch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantatyMeasurement
+{
+    public class YardToInch
+    {
+        /// <summary>
+        /// This method is checking that inch  and yard value is equal or not
+        /// </summary>
+        /// <param name="yard"></param>
+        /// <param name="inches"></param>
+        /// <returns></returns>
+        public bool? Checking_Equality_Of_Yard_And_Inch(Yard yard, Inches inches)
+        {
+            var inchValue = yard.GetValue() * 3 * 12;
+            if (inches.GetValue() == inchValue)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ QuantityMeasurementTest/YardToInchUnitTest.cs
@@ -0,0 +1,23 @@
+﻿
+namespace QuantityMeasurementTest
+{
+    using NUnit.Framework;
+    using QuantatyMeasurement;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class YardToInchUnitTest
+    {
+        /// <summary>
+        /// This method for checking that 1 inch and 1 yard is not equal
+        /// </summary>
+        [Test]
+        public void Checking_1Inch__And_1Yard_Equality()
+        {
+            Inches inches = new Inches(1);
+            Yard yard = new Yard(1);
+            YardToInch yardToInch = new YardToInch();
+            Assert.IsFalse(yardToInch.Checking_Equality_Of_Yard_And_Inch(yard, inches));
+        }
+    }
+}
