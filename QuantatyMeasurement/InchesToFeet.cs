e729e82ea23e039fdc860a3ac56be5c01111ac24
Anoop kumar <kanoop6401@gmail.com>
2020-04-03 11:13:29 +0530

    [Anoop] Added test case for converting inch into feet

---
+++ QuantatyMeasurement/InchesToFeet.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeetIntoInches.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Anoop kumar"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace QuantatyMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class InchesToFeet
+    {
+        /// <summary>
+        /// This method is converting Inche to Feet
+        /// </summary>
+        /// <param name="inches"></param>
+        /// <returns></returns>
+        public Feet Convering_Inch_To_Feet(Inches inches) 
+        {
+            var inchValue = inches.GetValue() / 12;
+            Feet feet = new Feet(inchValue);
+            return feet;
+        }
+    }
+}
---
+++ QuantityMeasurementTest/InchToFeetUnitTest.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeetInchesUnitTest.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Anoop kumar"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace QuantityMeasurementTest
+{
+    using NUnit.Framework;
+    using QuantatyMeasurement;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class InchToFeetUnitTest
+    {
+        /// <summary>
+        /// This method converting inch value into Feet value
+        /// </summary>
+        [Test]
+        public void Converting_Inch_To_Feet()
+        {
+            Inches inches = new Inches(12);
+            InchesToFeet inchToFeet = new InchesToFeet();
+            var feetValue=inchToFeet.Convering_Inch_To_Feet(inches);
+            Assert.AreEqual(feetValue.GetValue(), 1);
+        }
+
+    }
+}
