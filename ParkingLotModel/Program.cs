4448c298d12136bcafc958f45227cd99978ffd25
Anoop kumar <kanoop6401@gmail.com>
2020-04-25 17:04:29 +0530

    [Anoop] Model Class Added

---
+++ ParkingLotModel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLotModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //List<Model> cus = new List<Model>();
+            //cus.Add(new Model() { Id=101,SlotNumber="10b",OwnerName="Anoop",CarColor="red",CarModel="Maruti",VehicleNumber="up42SE1075",RatePerHour=20,CheckIn=DateTime.Now});
+            //foreach(var item in cus)
+            //{
+            //    Console.WriteLine(item.CheckIn);
+            //}
+        }
+    }
+}
