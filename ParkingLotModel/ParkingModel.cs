2e8be08afd9498dbbb7a92bc3d90b14a84840a5a
Anoop kumar <kanoop6401@gmail.com>
2020-04-28 12:15:04 +0530

    [Anoop] Refactor Model class

---
+++ ParkingLotModel/ParkingModel.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Model.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Anoop kumar"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ParkingLotModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+    public class ParkingModel
+    {
+        string slotNumber;
+        double ratePerHour;
+        DateTime checkIn;
+        DateTime checkOut;
+        string vehicleNumber;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string SlotNumber
+        {
+            get
+            {
+                return slotNumber;
+            }
+            set
+            {
+                slotNumber = value;
+            }
+        }
+        public double RatePerHour
+        {
+            get { return ratePerHour; }
+            set { ratePerHour = value; }
+        }
+        public DateTime CheckIn
+        {
+            get { return checkIn; }
+            set { checkIn = value; }
+        }
+        public DateTime CheckOut
+        {
+            get { return checkOut; }
+            set { checkOut = value; }
+        }
+        public string VehicleNumber
+        {
+            get { return vehicleNumber; }
+            set { vehicleNumber = value; }
+        }
+    }
+}
---
+++ Repository/Context/UserDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ParkingLotModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Repository.Context
+{
+    public class UserDBContext :DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options):base(options)
+        {
+        }
+        public DbSet<ParkingModel> Parkings
+        {
+            get;set;
+        }
+    }
+}
