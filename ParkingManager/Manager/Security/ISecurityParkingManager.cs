99e12e09442d738810d01ace69eb3d3f61fe83ef
Anoop kumar <kanoop6401@gmail.com>
2020-05-04 00:27:24 +0530

    [Anoop] Added Security parking manager

---
+++ ParkingManager/Manager/Security/ISecurityParkingManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingManager.Manager.Security
+{
+    public interface ISecurityParkingManager
+    {
+        Task<int> SecurityParking(ParkingModel parkingModel);
+        double SecurityUnparking(int slotNumber);
+    }
+}
---
+++ ParkingManager/Manager/Security/SecurityParkingManger.cs
@@ -0,0 +1,29 @@
+﻿using ParkingLotModel;
+using Repository.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingManager.Manager.Security
+{
+    public class SecurityParkingManger : ISecurityParkingManager
+    {
+        private readonly ISecurityParkingRopo securityParkingRopo;
+
+        public SecurityParkingManger(ISecurityParkingRopo securityParkingRopo)
+        {
+            this.securityParkingRopo = securityParkingRopo;
+        }
+
+        public Task<int> SecurityParking(ParkingModel parkingModel)
+        {
+            return securityParkingRopo.SecurityParking(parkingModel);
+        }
+
+        public double SecurityUnparking(int slotNumber)
+        {
+            return securityParkingRopo.SecurityUnparking(slotNumber);
+        }
+    }
+}
